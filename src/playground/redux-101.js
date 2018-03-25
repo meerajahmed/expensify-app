3301739229afbf7b2c4fd98fd492b718571cdc18
meerajahmed <meerajahmed@gmail.com>
2018-03-26 01:10:01 +0530

    subscribing and dynamic actions

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -7,24 +7,32 @@ const defaultState = {
 /**
  * Store: tracks data/state over time
  * Actions: allow us to change redux store
+ * Subscribe: watch state changes
  * */
 
 const store = createStore((state = defaultState, action) => {
   switch(action.type){
 
     case "INCREMENT":
+      const incrementBy = typeof action.incrementBy === "number" ? action.incrementBy : 1;
       return {
-        count: state.count + 1
+        count: state.count + incrementBy
       };
 
     case "DECREMENT":
+      const decrementBy = typeof action.decrementBy === "number" ? action.decrementBy : 1;
       return {
-        count: state.count - 1
+        count: state.count - decrementBy
       };
 
     case "RESET":
       return {
         count: 0
+      };
+      
+    case "SET":
+      return {
+        count: action.count
       };
 
     default:
@@ -32,24 +40,34 @@ const store = createStore((state = defaultState, action) => {
   }
 });
 
-console.log(store.getState());
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
 
 store.dispatch({
   type: "INCREMENT"
 });
-console.log(store.getState());
+
+store.dispatch({
+  type: "INCREMENT",
+  incrementBy: 5
+});
 
 store.dispatch({
   type: "DECREMENT"
 });
-console.log(store.getState());
 
 store.dispatch({
-  type: "INCREMENT"
+  type: "DECREMENT",
+  decrementBy: 10
 });
-console.log(store.getState());
+
+store.dispatch({
+  type: "SET",
+  count: 101
+});
 
 store.dispatch({
   type: "RESET"
 });
-console.log(store.getState());
