bfaaa3c2fe583bb328473df1bd0bd8b6ae3fafc7
meerajahmed <meerajahmed@gmail.com>
2018-03-25 12:01:14 +0530

    organizing routes

---
+++ src/routers/app-router.js
@@ -0,0 +1,28 @@
+import React from "react";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+
+import ExpenseDashboardPage from "../components/expense-dashboard-page";
+import AddExpensePage from "../components/add-expense-page";
+import EditExpensePage from "../components/edit-expense-page";
+import HelpPage from "../components/help-page";
+import NotFoundPage from "../components/not-found-page";
+import Header from "../components/header";
+
+// A <Router> may have only one child element. Hence we wrap routes in DIV
+
+const AppRouters = () => (
+  <BrowserRouter>
+    <div>
+      <Header/>
+      <Switch>
+        <Route path="/" component={ExpenseDashboardPage} exact={true} />
+        <Route path="/create" component={AddExpensePage} />
+        <Route path="/edit" component={EditExpensePage} />
+        <Route path="/help" component={HelpPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
+
+export default AppRouters;
