60e3d5ec3afd29026af2442a3c70165157ca715f
meerajahmed <meerajahmed@gmail.com>
2018-03-26 23:06:35 +0530

    Object destructuring

---
+++ src/playground/destructuring.js
@@ -0,0 +1,29 @@
+const person = {
+  name: "Andrew",
+  age: 26,
+  location: {
+    city: "Philadelphia",
+    temp: 88
+  }
+};
+
+// { } similar to named export syntax
+const { name: firstName = "Anonymous", age } = person;
+console.log(`${ firstName } is ${ age }.`);
+
+const { city, temp: temperature } = person.location;
+if( city && temperature ){
+  console.log(`It's ${temperature} in ${city}`);
+}
+
+const book = {
+  title: "Ego is the Enemy",
+  author: "Rayn Holiday",
+  publisher: {
+    name: "Penguin"
+  }
+};
+
+const { name: publisherName = "Self-Published"} = book.publisher;
+
+console.log(publisherName);
--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,4 +1,5 @@
 import { createStore } from "redux";
+import React from "react";
 
 const defaultState = {
   count: 0
