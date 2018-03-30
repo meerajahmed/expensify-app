82623652c6030c1a78e24ad26099b7dea81bcb26
meerajahmed <meerajahmed@gmail.com>
2018-03-30 23:09:42 +0530

    working with multiple reducers

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,5 +1,54 @@
 import { createStore, combineReducers } from "redux";
 
+/*
+* ADD_EXPENSE
+* REMOVE_EXPENSE
+* EDIT_EXPENSE
+*
+* SET_TEXT_FILTER
+* SROT_BY_DATE
+* SORT_BY_AMOUNT
+* SET_START_DATE
+* SET+END_DATE
+* */
+
+// expense reducer
+const expenseReducerDefaultState = [];
+
+const expenseReducer = ( state = expenseReducerDefaultState, action ) => {
+  switch (action.type) {
+
+    default:
+      return state;
+  }
+};
+
+// filter reducers
+const filterReducerDefaultState = {
+  text: "",
+  sortBy: "date",
+  startDate: undefined,
+  endDate: undefined
+};
+
+const filterReducer = ( state = filterReducerDefaultState, action ) => {
+  switch (action.type) {
+
+    default:
+      return state;
+  }
+};
+
+
+// Store creation
+const store = createStore(
+  combineReducers({
+    expenses: expenseReducer,
+    filters: filterReducer
+  })
+);
+console.log(store.getState());
+
 const demoState = {
   expenses: [{
     id: 'qwerty',
