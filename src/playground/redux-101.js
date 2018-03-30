2239169d239a4537b68815fb2494b78aeb0c89bb
meerajahmed <meerajahmed@gmail.com>
2018-03-30 19:21:55 +0530

    refactoring and organizing

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -19,7 +19,21 @@ const defaultState = {
 // replace ternary operator with default value of 1
 const incrementCount = ( { incrementBy = 1 } = {} ) => ({
   type: "INCREMENT",
-  incrementBy: incrementBy
+  incrementBy
+});
+
+const decrementCount = ( { decrementBy = 1} = {} ) => ({
+  type: "DECREMENT",
+  decrementBy
+});
+
+const setCount = ({ count }) => ({
+  type: "SET",
+  count
+});
+
+const resetCount = () => ({
+  type: "RESET"
 });
 
 const store = createStore((state = defaultState, action) => {
@@ -31,9 +45,8 @@ const store = createStore((state = defaultState, action) => {
       };
 
     case "DECREMENT":
-      const decrementBy = typeof action.decrementBy === "number" ? action.decrementBy : 1;
       return {
-        count: state.count - decrementBy
+        count: state.count - action.decrementBy
       };
 
     case "RESET":
@@ -60,20 +73,10 @@ store.dispatch(incrementCount());
 
 store.dispatch(incrementCount({incrementBy: 5}));
 
-store.dispatch({
-  type: "DECREMENT"
-});
+store.dispatch(decrementCount());
 
-store.dispatch({
-  type: "DECREMENT",
-  decrementBy: 10
-});
+store.dispatch(decrementCount({ decrementBy: 10}));
 
-store.dispatch({
-  type: "SET",
-  count: 101
-});
+store.dispatch(setCount({ count: 101}));
 
-store.dispatch({
-  type: "RESET"
-});
+store.dispatch(resetCount());
