d921d8eecb0842e543f86788095eb13da817edff
meerajahmed <meerajahmed@gmail.com>
2018-03-31 13:11:21 +0530

    ES6 spread operator in reducers

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -44,12 +44,28 @@ const setTextFilter = (text = "") => ({
   text
 });
 
-/*
-* SORT_BY_DATE
-* SORT_BY_AMOUNT
-* SET_START_DATE
-* SET+END_DATE
-* */
+// SORT_BY_AMOUNT
+const sortByAmount = () => ({
+  type: "SORT_BY_AMOUNT"
+});
+
+// SORT_BY_DATE
+const sortByDate = () => ({
+  type: "SORT_BY_DATE"
+});
+
+// SET_START_DATE
+const setStartDate = (startDate) => ({
+  type: "SET_START_DATE",
+  startDate
+});
+
+// SET_END_DATE
+const setEndDate = (endDate) => ({
+  type: "SET_END_DATE",
+  endDate
+});
+
 
 // expense reducer
 const expenseReducerDefaultState = [];
@@ -96,6 +112,30 @@ const filterReducer = ( state = filterReducerDefaultState, action ) => {
         text: action.text
       };
 
+    case "SORT_BY_AMOUNT":
+      return {
+        ...state,
+        sortBy: "amount"
+      };
+
+    case "SORT_BY_DATE":
+      return {
+        ...state,
+        sortBy: "date"
+      };
+
+    case "SET_START_DATE":
+      return {
+        ...state,
+        startDate: action.startDate
+      };
+
+    case "SET_END_DATE":
+      return {
+        ...state,
+        endDate: action.endDate
+      };
+
     default:
       return state;
   }
@@ -126,6 +166,13 @@ store.dispatch(updateExpense(expenseTwo.expense.id, { amount: 70 }));
 store.dispatch(setTextFilter("rent"));
 store.dispatch(setTextFilter());
 
+store.dispatch(sortByAmount());
+store.dispatch(sortByDate());
+
+store.dispatch(setStartDate(125));
+store.dispatch(setStartDate()); // by default startDate = undefined
+
+store.dispatch(setEndDate(1250));
 
 const demoState = {
   expenses: [{
