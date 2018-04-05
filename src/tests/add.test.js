418a9bd94ca4db15e13adc16fa4445276e9a357b
meerajahmed <meerajahmed@gmail.com>
2018-04-06 00:41:28 +0530

    setting up Jest

--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -1,3 +1,12 @@
-/**
- * Created by mahme4 on 4/6/2018.
- */
+const add = (a, b) => a + b;
+const generateGreeting = (name) => `Hello ${name}`;
+
+//test case
+test("should add two numbers", () => {
+  const result = add(3, 4);
+  expect(result).toBe(7);
+});
+
+test("should generate greeting", () => {
+  expect(generateGreeting("Andrew")).toBe("Hello Andrew");
+});
