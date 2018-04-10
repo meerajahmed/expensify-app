285166840c3ba32a70fbbc6c8277eb6a8b6f34e8
meerajahmed <meerajahmed@gmail.com>
2018-04-10 23:01:53 +0530

    mocking libraries with jest

--- src/tests/components/expense-form.test.js
+++ src/tests/components/expense-form.test.js
@@ -7,3 +7,8 @@ test("should render ExpenseForm correctly", () => {
   const wrapper = shallow(<ExpenseForm />);
   expect(wrapper).toMatchSnapshot();
 });
+
+test("should render ExpenseForm correctly with expense data", () => {
+  const wrapper = shallow(<ExpenseForm expense={expenses[1]} />);
+  expect(wrapper).toMatchSnapshot();
+});
