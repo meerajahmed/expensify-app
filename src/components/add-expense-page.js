fbe038e3b344e0fe81229493ea6fe7955fdf52f6
meerajahmed <meerajahmed@gmail.com>
2018-04-14 23:19:38 +0530

    testing addExpense page

--- src/components/add-expense-page.js
+++ src/components/add-expense-page.js
@@ -3,13 +3,44 @@ import { connect } from "react-redux";
 import ExpenseForm from "./expense-form";
 import { addExpense } from "../actions/expenses";
 
+/*
 const AddExpensePage = (props) => (
   <div>
     <ExpenseForm onSubmit={(expense) => {
-       props.dispatch(addExpense(expense));
+       //props.dispatch(addExpense(expense));
+       props.onSubmit(expense); // easy to spy on expense argument
        props.history.push('/');
     }} />
   </div>
 );
+*/
 
-export default connect()(AddExpensePage);
+export class AddExpensePage extends React.Component {
+
+  onSubmit = (expense) => {
+    //props.dispatch(addExpense(expense));
+    this.props.onSubmit(expense); // easy to spy on expense argument
+    this.props.history.push('/');
+  };
+
+  render() {
+    return (
+      <div>
+        <ExpenseForm onSubmit={this.onSubmit} />
+      </div>
+    );
+  }
+}
+
+//
+/*
+* Its difficult to spy addExpense function call argument.
+* So we abstract dispatch (addExpense) call from component using mapDispatchToProps
+* */
+
+const mapDispatchToProps = (dispatch) => (
+  {
+    onSubmit: (expense) => dispatch(addExpense(expense))
+  }
+);
+export default connect(undefined, mapDispatchToProps)(AddExpensePage);
