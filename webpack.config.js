4969359f6bbe5879cbb59aa797672071c164e248
meerajahmed <meerajahmed@gmail.com>
2018-04-16 00:55:21 +0530

    setup webpack production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,69 @@
 const path  = require("path");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-  entry: "./src/app.js",
-  output: {
-    path: path.join(__dirname, "public"),
-    filename: "bundle.js"
-  },
-  module: {
-    rules: [
-      {
-        loader: "babel-loader",
-        test: /\.js$/,
-        exclude: /node_modules/
-      },
-      /*
-       * css-loader : use css in JS
-       * style-loader : inject style in DOM
-       * sass-loader: import scss file in JS
-       * node-sass : transform .scss to .css
-       * use : when we have multiple loader
-       * */
-      {
-        use: ["style-loader", "css-loader", "sass-loader"],
-        test: /\.s?css$/
-      }
-    ]
-  },
-  devtool: "cheap-module-eval-source-map",
-  devServer: {
-    contentBase: path.join(__dirname, "public"),
-    // fallback to /index.html
-    historyApiFallback: true,
-  }
-  /*
-  * production: source-map
-  * development: eval-source-map, cheap-module-eval-source-map
-  * */
+module.exports = (env, args) => {
+  const isProduction = env === "production";
+  const extractCSS = new ExtractTextPlugin("styles.css");
+
+  return {
+    entry: "./src/app.js",
+    output: {
+      path: path.join(__dirname, "public"),
+      filename: "bundle.js"
+    },
+    module: {
+      rules: [
+        {
+          loader: "babel-loader",
+          test: /\.js$/,
+          exclude: /node_modules/
+        },
+        /*
+         * css-loader : use css in JS
+         * style-loader : inject style in DOM
+         * sass-loader: import scss file in JS
+         * node-sass : transform .scss to .css
+         * use : when we have multiple loader
+         * */
+        /*{
+          use: ["style-loader", "css-loader", "sass-loader"],
+          test: /\.s?css$/
+        }*/
+        {
+          test: /\.s?css$/,
+          use: extractCSS.extract({
+            use: [
+              {
+                loader: "css-loader",
+                options: {
+                  sourceMap: true
+                }
+              },
+              {
+                loader: "sass-loader",
+                options: {
+                  sourceMap: true
+                }
+              }
+            ]
+          })
+        }
+      ]
+    },
+    plugins: [
+      extractCSS
+    ],
+    devtool: isProduction ? "source-map" : "inline-source-map",
+    devServer: {
+      contentBase: path.join(__dirname, "public"),
+      // fallback to /index.html
+      historyApiFallback: true,
+    }
+    /*
+     * production: source-map
+     * development: eval-source-map, cheap-module-eval-source-map
+     * */
+  };
 };
 // loader -> customise behavior of webpack when a file is loaded
 // eg: .scss -> .css
