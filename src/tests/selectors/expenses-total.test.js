03ac7024436e5ac2726efd21eb3dadbd37af1f28
meerajahmed <meerajahmed@gmail.com>
2018-04-21 22:45:24 +0530

    adding total expenses

--- src/tests/selectors/expenses-total.test.js
+++ src/tests/selectors/expenses-total.test.js
@@ -1,3 +1,17 @@
-/**
- * Created by mahme4 on 4/21/2018.
- */
+import selectorExpensesTotal from "../../selectors/expenses-total";
+import expenses from "../fixtures/expenses";
+
+test("should return 0 if no expenses", () => {
+  const total = selectorExpensesTotal();
+  expect(total).toBe(0);
+});
+
+test("should correctly add up a single expense", () => {
+  const total = selectorExpensesTotal([expenses[0]]);
+  expect(total).toBe(195);
+});
+
+test("should correctly add up multiple expenses", () => {
+  const total = selectorExpensesTotal(expenses);
+  expect(total).toBe(114195);
+});
